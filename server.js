cffe0d339fc6bfed3f02cbb28a0ce92167ef23ae
BigMouthInc <144144777+BigMouthInc@users.noreply.github.com>
2025-09-26 16:33:17 -0400

    Dillon branch complete

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/src/index.html')
 })
 
+app.get('/dillon', (req, res) =>{
+    res.sendFile(_dirname + '/src/dillon.html')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
