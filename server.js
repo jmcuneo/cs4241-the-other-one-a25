4ffbddc6ab83340abccd65e0e546d47d9f61e907
Akaash-Walker <awalker7179@gmail.com>
2025-09-26 16:30:27 -0400

    add my data and chart

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/src/index.html')
 })
 
+app.get('/akaash', (req, res) => {
+    res.sendFile(__dirname + '/src/akaash.html')
+})
+
+app.get('/data.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.sendFile((__dirname + '/data.json'));
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
