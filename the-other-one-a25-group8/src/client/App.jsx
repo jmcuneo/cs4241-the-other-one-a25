19025e0a9f1cd18404373f3fa75ac8c7989aacdb
Ayush <ayushk2023@gmail.com>
2025-09-26 16:44:11 -0400

    I tried :(

--- the-other-one-a25-group8/src/client/App.jsx
+++ the-other-one-a25-group8/src/client/App.jsx
@@ -1,24 +1,55 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import React from "react"
 
-function App() {
-  const [count, setCount] = useState(0);
+class Hurricane extends React.Component {
+  // our .render() method creates a block of HTML using the .jsx format
+  render() {
+    return <tbody>{this.props.name} : 
+      <tr name={this.props.Name} year={this.props.year} windspeed={this.props.windspeed} airpressure={this.props.airpressure} />
+    </tbody>
+  }
+  // call this method when the checkbox for this component is clicked
+  // change(e) {
+  //   this.props.onclick( this.props.name, e.target.checked )
+  // }
+}
 
-  return (
-    <div className="App">
-      <h1>Group 8 Assignment 4</h1>
+class App extends React.Component {
+  constructor( props ) {
+    super( props )
+    // initialize our state
+    this.state = { appdata:[] }
+    this.load()
+  }
+
+  // load in our data from the server
+  load() {
+    fetch( '/read', { method:'get', 'no-cors':true })
+      .then( response => response.json() )
+      .then( json => {
+         this.setState({ appdata:json }) 
+      })
+  }
+
+
+  render() {
+    return (
+      <div>
       <form>
         <input type="text" id="stormname" placeholder="Storm name here"></input>
         <input type="text" id="year" placeholder="Year here"></input>
         <input type="text" id="windspeed" placeholder="Wind speed here (mph)"></input>
         <input type="text" id="airpressure" placeholder="Air pressure here (mbar)"></input>
-        <button class="changer">submit</button>
-        <button id="delete" class="changer">delete</button>
-
+        <button className="changer" >submit</button>
+        <button id="delete" className="changer">delete</button>
       </form>
-    </div>
-  );
+      <table id="dataset">
+          <tbody>
+          { this.state.appdata.map( (appdata, i) => <Hurricane key={i} Name={Hurricane.Name} Year={Hurricane.Year} Windspeed={Hurricane.Windspeed} Airpressure={Hurricane.Airpressure} /> ) }
+          </tbody>
+      </table>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- the-other-one-a25-group8/src/server/main.js
+++ the-other-one-a25-group8/src/server/main.js
@@ -1,26 +1,64 @@
+// import express from "express"
+// import ViteExpress from "vite-express"
+
+// const app = express()
+// const appdata = ["hiudasicni"]
+
+// app.use(express.json())
+// app.get("/", (request, response) => response.json(appdata))
+// app.post("/data", (request, response) => {
+//   appdata.push(req.body)
+//   response.json(appdata)
+// })
+
+// // const sshws = function( mph ) {
+// //   if (mph >= 157){
+// //     return "Category 5 Major Hurricane";
+// //   }
+// //   else if (mph >= 130){
+// //     return "Category 4 Major Hurricane";
+// //   }
+// //   else if (mph >= 111){
+// //     return "Category 3 Major Hurricane";
+// //   }
+// //   else if (mph >= 96){
+// //     return "Category 2 Hurricane";
+// //   }
+// //   else if (mph >= 74){
+// //     return "Category 1 Hurricane";
+// //   }
+// //   else if (mph >= 39){
+// //     return "Tropical Storm";
+// //   }
+// //   else{
+// //     return "Tropical Depression";
+// //   }
+// // }
+
+// ViteExpress.listen(app, 3000)
+
 import express from  'express'
 import ViteExpress from 'vite-express'
 
 const app = express()
 
-const todos = [
-  { name:'buy groceries', completed:false }
+const appdata = [
+  {"Name": "Ayush", "Year": 2025, "Windspeed": 25, "Airpressure": 50}
 ]
 
 app.use( express.json() )
 
-app.get( '/read', ( req, res ) => res.json( todos ) )
+app.get( '/read', ( req, res ) => res.json( appdata ) )
 
 app.post( '/add', ( req,res ) => {
-  todos.push( req.body )
-  res.json( todos )
+  appdata.push( req.body )
+  res.json( appdata )
 })
-
-app.post( '/change', function( req,res ) {
-  const idx = todos.findIndex( v => v.name === req.body.name )
-  todos[ idx ].completed = req.body.completed
+// app.post( '/change', function( req,res ) {
+//   const idx = todos.findIndex( v => v.name === req.body.name )
+//   appdata[ idx ].completed = req.body.completed
   
-  res.sendStatus( 200 )
-})
+//   res.sendStatus( 200 )
+// })
 
 ViteExpress.listen( app, 3000 )
