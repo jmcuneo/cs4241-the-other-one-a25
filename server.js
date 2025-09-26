965b7c94ce00c20f12ccf033d14980128d7d1f4a
Owen Hart <owenvhart@gmail.com>
2025-09-26 15:59:23 -0400

    rewrites server for express

--- server.js
+++ server.js
@@ -9,9 +9,29 @@ const appdata = [
 
 app.use( express.json() )
 
+app.get('/appdata', (req, res) => {
+    res.writeHeader( 200, { "Content-Type": JSON })
+    res.end( JSON.stringify(appdata) );
+})
+
 app.post( '/remove', (req, res) => {
 
-    res.end(JSON.stringify( req.body ) )
+    const rowToRemove = req.body
+
+    appdata.splice( rowToRemove, 1 )
+
+    req.writeHead( 200, "OK", {"Content-Type": JSON })
+    res.end(JSON.stringify(appdata))
+})
+
+app.post('/add', (req, res) => {
+    const data = req.body
+
+    appdata.push( data )
+    console.log( appdata )
+
+    res.writeHead( 200, "OK", {"Content-Type": JSON })
+    res.end(JSON.stringify(appdata))
 })
 
 ViteExpress.listen( app, 3000 )
