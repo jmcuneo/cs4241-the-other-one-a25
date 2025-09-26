f197e93edc7f7aeea1c343a752a528f548ac7e6d
Jack Richard <jack@jackrichard.me>
2025-09-26 15:28:20 -0400

    Current changes

---
+++ script.js
@@ -0,0 +1,18 @@
+const playButton = document.querySelector("#play-button");
+
+let audioCtx = new AudioContext();
+let track = audioCtx.createBufferSource();
+
+fetch("cat-meow-401729.mp3")
+    .then(response => response.arrayBuffer())
+    .then(buffer => audioCtx.decodeAudioData(buffer))
+    .then(buffer => {
+        track.buffer = buffer;
+
+        const node = new StereoPannerNode(audioCtx, { pan: -1 });
+        node.connect(audioCtx.destination);
+
+        track.connect(node);
+    });
+
+playButton.addEventListener("click", () => track.start(0));
