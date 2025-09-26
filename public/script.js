fe4fea2c902799b279a3a97e86f1f1c96099fc95
Jack Richard <jack@jackrichard.me>
2025-09-26 15:51:21 -0400

    Changes again

--- public/script.js
+++ public/script.js
@@ -1,13 +1,11 @@
-const playButton = document.querySelector("#play-button");
-
-let audioCtx = new AudioContext();
+const audioCtx = new AudioContext();
 
 async function loadSound(fileName, location) {
     let node = audioCtx.createBufferSource();
 
     let response = await fetch(fileName);
-    let arraybuffer = await response.arrayBuffer();
-    node.buffer = await audioCtx.decodeAudioData(arraybuffer);
+    let buffer = await response.arrayBuffer();
+    node.buffer = await audioCtx.decodeAudioData(buffer);
 
     const panner = audioCtx.createStereoPanner();
     panner.pan.value = location;
@@ -18,8 +16,10 @@ async function loadSound(fileName, location) {
     return node;
 }
 
-let sound;
-loadSound("audioFiles/cat-meow-401729.mp3", -1)
-    .then((node) => sound = node);
+const catButton = document.querySelector("#cat");
+const cowButton = document.querySelector("#cow");
+const pigButton = document.querySelector("#pig");
 
-playButton.addEventListener("click", () => sound.start(0));
+catButton.addEventListener("click", () => loadSound("audioFiles/cat-meow-401729.mp3", 0).then(x => x.start()));
+cowButton.addEventListener("click", () => loadSound("audioFiles/cow-mooing-343423.mp3", 1).then(x => x.start()));
+pigButton.addEventListener("click", () => loadSound("audioFiles/pig-oink-47167.mp3", -1).then(x => x.start()));
