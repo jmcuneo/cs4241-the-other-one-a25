7969d96d48d1f14526a7f14e95db25e2104efcb8
AddecheRyan <rnaddeche@wpi.edu>
2025-09-26 16:12:42 -0400

    added visualization

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 const playButton = document.querySelector("#play-button");
 
 let audioCtx = new AudioContext();
+let analyser = audioCtx.createAnalyser();
 
 async function loadSound(fileName, location) {
     let node = audioCtx.createBufferSource();
@@ -13,7 +14,9 @@ async function loadSound(fileName, location) {
     panner.pan.value = location;
     panner.connect(audioCtx.destination);
 
-    node.connect(panner);
+    analyser.connect(panner);
+
+    node.connect(analyser);
 
     return node;
 }
@@ -23,3 +26,54 @@ loadSound("audioFiles/cat-meow-401729.mp3", -1)
     .then((node) => sound = node);
 
 playButton.addEventListener("click", () => sound.start(0));
+
+
+// Visualization of audio section 
+
+
+
+analyser.fftSize = 2048;
+const bufferLength = analyser.frequencyBinCount;
+const dataArray = new Uint8Array(bufferLength);
+analyser.getByteTimeDomainData(dataArray);
+
+const canvas = document.getElementById("oscilloscope");
+const canvasCtx = canvas.getContext("2d");
+
+const WIDTH = canvas.width;
+const HEIGHT = canvas.height;
+
+function draw() {
+  drawVisual = requestAnimationFrame(draw);
+
+  analyser.getByteTimeDomainData(dataArray);
+
+  canvasCtx.fillStyle = "rgb(200 200 200)";
+  canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+  canvasCtx.lineWidth = 2;
+  canvasCtx.strokeStyle = "rgb(0 0 0)";
+
+  canvasCtx.beginPath();
+
+  const sliceWidth = (WIDTH * 1.0) / bufferLength;
+  let x = 0;
+
+  for (let i = 0; i < bufferLength; i++) {
+    const v = dataArray[i] / 128.0;
+    const y = (v * HEIGHT) / 2;
+
+    if (i === 0) {
+      canvasCtx.moveTo(x, y);
+    } else {
+      canvasCtx.lineTo(x, y);
+    }
+
+    x += sliceWidth;
+  }
+
+  canvasCtx.lineTo(canvas.width, canvas.height / 2);
+  canvasCtx.stroke();
+}
+
+draw();
