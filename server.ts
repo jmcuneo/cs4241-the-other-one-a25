44c78e72d841eb1b9d992124b3943e9ce32efebf
Akaash-Walker <awalker7179@gmail.com>
2025-09-26 15:30:56 -0400

    add express server with index.html

---
+++ server.ts
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req: any, res: any) => {
+    res.sendFile(__dirname + '/src/index.html')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
